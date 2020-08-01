385a0c88df3a537cc00a81310d49c742b30902d9
Şerif <gungoronline@gmail.com>
2020-08-01 23:53:54 +0300

    Add files via upload

--- app/src/main/java/com/gungoronline/photto/MainActivity.java
+++ app/src/main/java/com/gungoronline/photto/MainActivity.java
@@ -1,15 +1,27 @@
 package com.gungoronline.photto;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.gungoronline.photto.Model.BitmapType;
+import com.gungoronline.photto.Model.Photto;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        File[] files = new File[]{new File("/storage/emulated/0/picture1.jpg"),new File("/storage/emulated/0/picture2.jpg")};
+        Bitmap b = new Photto.BitmapBuilder(getApplicationContext(),files,"", BitmapType.TYPE_3).build();
+        ImageView iv = new ImageView(getApplicationContext());
+        iv.setImageBitmap(b);
 
+        setContentView(iv);//R.layout.activity_main
     }
 }
